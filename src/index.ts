664075409c075a876793dc0e49af43fe45d8afd6
zlumer <zlumer@gmail.com>
2021-01-21 20:22:48 +0300

    cli

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
-import express from "express"
+import { collectWarnings as collectErrors, readTree } from "./readdir"
+import { createServer } from "./server"
+import arg from "arg"
 
-let app = express()
+const args = arg({
+	"--port": Number,
+	"-p": "--port",
+})
 
-let PORT = process.env.PORT || 3006
+const PATH = args._[0]
+if (!PATH)
+{
+	console.error(`input directory should be provided!`)
+	process.exit(1)
+}
+
+console.log(`loading json from ${PATH}`)
+
+let info = readTree(PATH)
+
+for (let x of collectErrors(info))
+	console.error(`error: ${x}`)
+
+let app = createServer(info.json)
+
+let PORT = args["--port"] || process.env.PORT || 3000
 
 console.log(`starting server on ${PORT}...`)
 app.listen(PORT, () =>
